fe23a70d8a54e9ca363957cc341a76a9330c1bb9
Hajoon <dutls0124@naver.com>
2025-09-06 22:15:02 +0900

    RedisConfig 전부 주석

--- hr-service/src/main/java/com/playdata/hrservice/common/config/RedisConfig.java
+++ hr-service/src/main/java/com/playdata/hrservice/common/config/RedisConfig.java
@@ -1,64 +1,64 @@
-package com.playdata.hrservice.common.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-public class RedisConfig {
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Bean
-    public ObjectMapper redisObjectMapper() {
-        return new ObjectMapper()
-                .registerModule(new JavaTimeModule())               // LocalDate 지원
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-    }
-
-    // Redis 서버와의 연결을 설정하는 역할을 하는 RedisConnectionFactory
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration
-                = new RedisStandaloneConfiguration();
-        configuration.setHostName(host);
-        configuration.setPort(port);
-        configuration.setDatabase(1); // 1번 DB 사용하겠다. default -> 0
-        return new LettuceConnectionFactory(configuration);
-    }
-
-    // spring과 redis가 상호작용할 때 redis key, value의 형식을 정의
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(
-            RedisConnectionFactory factory,
-            ObjectMapper redisObjectMapper
-    ) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-
-        // Redis의 키를 문자열로 직렬화
-        template.setKeySerializer(new StringRedisSerializer());
-
-        GenericJackson2JsonRedisSerializer serializer =
-                new GenericJackson2JsonRedisSerializer(redisObjectMapper);
-        // value는 JSON으로 직렬화
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
-
-        // Redis 접속 정보를 template에게 전달해서 위에서 설정한 정보로 접속 후 사용
-        template.setConnectionFactory(factory);
-
-        return template;
-    }
-}
+//package com.playdata.hrservice.common.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.SerializationFeature;
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//@Configuration
+//public class RedisConfig {
+//    @Value("${spring.data.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.data.redis.port}")
+//    private int port;
+//
+//    @Bean
+//    public ObjectMapper redisObjectMapper() {
+//        return new ObjectMapper()
+//                .registerModule(new JavaTimeModule())               // LocalDate 지원
+//                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+//    }
+//
+//    // Redis 서버와의 연결을 설정하는 역할을 하는 RedisConnectionFactory
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration configuration
+//                = new RedisStandaloneConfiguration();
+//        configuration.setHostName(host);
+//        configuration.setPort(port);
+//        configuration.setDatabase(1); // 1번 DB 사용하겠다. default -> 0
+//        return new LettuceConnectionFactory(configuration);
+//    }
+//
+//    // spring과 redis가 상호작용할 때 redis key, value의 형식을 정의
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate(
+//            RedisConnectionFactory factory,
+//            ObjectMapper redisObjectMapper
+//    ) {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//
+//        // Redis의 키를 문자열로 직렬화
+//        template.setKeySerializer(new StringRedisSerializer());
+//
+//        GenericJackson2JsonRedisSerializer serializer =
+//                new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+//        // value는 JSON으로 직렬화
+//        template.setValueSerializer(serializer);
+//        template.setHashValueSerializer(serializer);
+//
+//        // Redis 접속 정보를 template에게 전달해서 위에서 설정한 정보로 접속 후 사용
+//        template.setConnectionFactory(factory);
+//
+//        return template;
+//    }
+//}

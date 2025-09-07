82b31193242f93be16403d54c988b89c9ecc1a9b
hajoon <stravinskky@naver.com>
2025-09-07 14:36:42 +0900

    RedisConfig 및 yml 수정

--- notice-service/src/main/java/com/playdata/noticeservice/common/config/RedisConfig.java
+++ notice-service/src/main/java/com/playdata/noticeservice/common/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.playdata.noticeservice.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -12,25 +13,26 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
+//    @Value("${spring.data.redis.host}")
+//    private String host;
+//
+//    @Value("${spring.data.redis.port}")
+//    private int port;
 
     // Redis 서버와의 연결을 설정하는 역할을 하는 RedisConnectionFactory
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration
-                = new RedisStandaloneConfiguration();
-        configuration.setHostName(host);
-        configuration.setPort(port);
-        configuration.setDatabase(1); // 1번 DB 사용하겠다. default -> 0
-        return new LettuceConnectionFactory(configuration);
-    }
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration configuration
+//                = new RedisStandaloneConfiguration();
+//        configuration.setHostName(host);
+//        configuration.setPort(port);
+//        configuration.setDatabase(1); // 1번 DB 사용하겠다. default -> 0
+//        return new LettuceConnectionFactory(configuration);
+//    }
 
     // spring과 redis가 상호작용할 때 redis key, value의 형식을 정의
     @Bean
+    @ConditionalOnMissingBean(name = "redisTemplate")
     public RedisTemplate<String, Object> redisTemplate(
             RedisConnectionFactory factory
     ) {
